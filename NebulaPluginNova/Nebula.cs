940f43fe1a5e4c625eac549060103825b65e5f9a
ZsFabTest <a1234567890001919@outlook.com>
2024-08-07 14:03:14 +0800

    Pre-Release v2.0.0.3 v1

--- NebulaPluginNova/Nebula.cs
+++ NebulaPluginNova/Nebula.cs
@@ -42,10 +42,10 @@ public class NebulaPlugin : BasePlugin
     public const string PluginVersion = "2.0.0";
 
     //public const string VisualVersion = "v2.0.0.2";
-    public const string VisualVersion = "Snapshot 24.08.07c";
+    public const string VisualVersion = "Pre-Release v2.0.0.3 v1";
 
     public const string PluginEpochStr = "100";
-    public const string PluginBuildNumStr = "1048";
+    public const string PluginBuildNumStr = "1050";
     public static readonly int PluginEpoch = int.Parse(PluginEpochStr);
     public static readonly int PluginBuildNum = int.Parse(PluginBuildNumStr);
     public const bool GuardVanillaLangData = false;

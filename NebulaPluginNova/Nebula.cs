dc266f84cf56805c97ce9fb6fa9b144a2bc98232
ZsFabTest <a1234567890001919@outlook.com>
2024-09-01 08:21:52 +0800

    24w35b

--- NebulaPluginNova/Nebula.cs
+++ NebulaPluginNova/Nebula.cs
@@ -42,7 +42,7 @@ public class NebulaPlugin : BasePlugin
     public const string PluginVersion = "2.0.1";
 
     //public const string VisualVersion = "v2.0.1";
-    public const string VisualVersion = "Snapshot 24w36a";
+    public const string VisualVersion = "Snapshot 24w35b";
 
     public const string PluginEpochStr = "102";
     public const string PluginBuildNumStr = "1115";

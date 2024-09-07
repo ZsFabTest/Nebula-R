d345140102723af2ef9a0ab96bfb184690c3b120
ZsFabTest <a1234567890001919@outlook.com>
2024-09-07 21:52:18 +0800

    0907

---
+++ AUServerTool/Main.cs
@@ -0,0 +1,105 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using Il2CppInterop.Runtime.InteropTypes;
+using UnityEngine;
+using System.Linq.Expressions;
+using UnityEngine.Events;
+using System.Reflection;
+using Cpp2IL.Core.Extensions;
+
+namespace AUServerTool;
+
+[BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+[BepInProcess("Among Us.exe")]
+public class AUServerToolPlugin : BasePlugin
+{
+    public const string AmongUsVersion = "2024.8.13s";
+    public const string PluginGuid = "com.github.zsfabtest.aust";
+    public const string PluginName = "Among Us Server Manage Tool";
+    public const string PluginVersion = "1.0.0";
+
+    public override void Load()
+    {
+        new Harmony(PluginGuid).PatchAll();
+        if (!File.Exists(Path.Combine(Paths.GameRootPath, "RegionInfo") + "/regionInfo.json"))
+        {
+            var bytes = Assembly.GetExecutingAssembly().GetManifestResourceStream("AUST.Resources.regionInfo.json")!.ReadBytes();
+            if (!Directory.Exists(Path.Combine(Paths.GameRootPath, "RegionInfo")))
+                Directory.CreateDirectory(Path.Combine(Paths.GameRootPath, "RegionInfo"));
+            var fileStream = File.Create(Path.Combine(Paths.GameRootPath, "RegionInfo") + "/regionInfo.json");
+            fileStream.Write(bytes, 0, bytes.Length);
+            fileStream.Close();
+        }
+    }
+}
+
+public static class Il2CppHelpers
+{
+    private static class CastHelper<T> where T : Il2CppObjectBase
+    {
+        public static Func<IntPtr, T> Cast;
+        static CastHelper()
+        {
+            var constructor = typeof(T).GetConstructor(new[] { typeof(IntPtr) });
+            var ptr = Expression.Parameter(typeof(IntPtr));
+            var create = Expression.New(constructor!, ptr);
+            var lambda = Expression.Lambda<Func<IntPtr, T>>(create, ptr);
+            Cast = lambda.Compile();
+        }
+    }
+
+    public static T CastFast<T>(this Il2CppObjectBase obj) where T : Il2CppObjectBase
+    {
+        if (obj is T casted) return casted;
+        return obj.Pointer.CastFast<T>();
+    }
+
+    public static T CastFast<T>(this IntPtr ptr) where T : Il2CppObjectBase
+    {
+        return CastHelper<T>.Cast(ptr);
+    }
+}
+
+[HarmonyPatch(typeof(RegionMenu), nameof(RegionMenu.OnEnable))]
+public static class RegionMenuOnEnablePatch
+{
+    public static void Postfix(RegionMenu __instance)
+    {
+        DestroyableSingleton<ServerManager>.Instance.LoadServers();
+        foreach (var button in __instance.ButtonPool.activeChildren)
+        {
+            var serverButton = button.CastFast<ServerListButton>();
+            serverButton.Button.OnClick.RemoveAllListeners();
+            serverButton.Button.OnClick.AddListener((UnityAction)(() =>
+            {
+                var region = ServerManager.Instance.AvailableRegions.FirstOrDefault(region => region.Name.Equals(serverButton.textTranslator.defaultStr));
+                if (region != null) __instance.ChooseOption(region);
+            }));
+
+        }
+
+        Transform back = __instance.ButtonPool.transform.FindChild("Backdrop");
+        back.transform.localScale *= 10f;
+        var oldScroller = __instance.ButtonPool.transform.parent.gameObject.GetComponent<Scroller>;
+        if (oldScroller != null) oldScroller = null;
+        Scroller RegionMenuScroller = __instance.ButtonPool.transform.parent.gameObject.AddComponent<Scroller>();
+        RegionMenuScroller.Inner = __instance.ButtonPool.transform;
+        RegionMenuScroller.MouseMustBeOverToScroll = true;
+        RegionMenuScroller.ClickMask = back.GetComponent<BoxCollider2D>();
+        RegionMenuScroller.ScrollWheelSpeed = 0.5f;
+        RegionMenuScroller.SetYBoundsMin(0f);
+        RegionMenuScroller.SetYBoundsMax(__instance.ButtonPool.poolSize * 0.222f);
+        RegionMenuScroller.allowY = true;
+        RegionMenuScroller.allowX = false;
+    }
+}
+
+[HarmonyPatch(typeof(ServerManager), nameof(ServerManager.LoadServers))]
+public static class LoadServersPatch
+{
+    public static void Prefix(ServerManager __instance)
+    {
+        __instance.serverInfoFileJson = Path.Combine(Paths.GameRootPath, "RegionInfo", "regionInfo.json");
+    }
+}
---
+++ TianMengLucky/Scripts/TianMengLucky.cs
@@ -0,0 +1,61 @@
+﻿using Virial.Assignable;
+using Virial.Events.Game;
+using Virial.Events.Player;
+
+public class TianMengLucky : DefinedAllocatableModifierTemplate, DefinedAllocatableModifier
+{
+    private TianMengLucky() : base("tianMengLucky", "TML", new(11, 45, 14), []) { }
+
+    RuntimeModifier RuntimeAssignableGenerator<RuntimeModifier>.CreateInstance(Virial.Game.Player player, int[] arguments) => new Instance(player);
+
+    public static TianMengLucky MyRole = new TianMengLucky();
+    public class Instance : RuntimeAssignableTemplate, RuntimeModifier
+    {
+        DefinedModifier RuntimeModifier.Modifier => MyRole;
+
+        public Instance(Virial.Game.Player player) : base(player) { }
+
+        void RuntimeAssignable.OnActivated() { }
+
+        [Local]
+        void OnCriteriaUpdate(CriteriaUpdateEvent ev)
+        {
+            if (!MyPlayer.IsDead && MyPlayer.Role.Role.Category is RoleCategory.CrewmateRole && ev.CriterialGameEnd == NebulaGameEnd.CrewmateWin)
+                ev.BlockWinning(true);
+        }
+
+        [Local]
+        void BlockWin(PlayerBlockWinEvent ev)
+        {
+            if (MyPlayer.Role.Role.Category is RoleCategory.CrewmateRole && 
+                ev.GameEnd == NebulaGameEnd.CrewmateWin)
+                ev.SetBlockedIf(true);
+            if (MyPlayer.Role.Role.Category is RoleCategory.ImpostorRole && 
+              PlayerControl.AllPlayerControls.GetFastEnumerator().Count(
+                (p) => !p.Data.IsDead && 
+                  p.GetModInfo()?.Role.Role.Category is RoleCategory.ImpostorRole) > 1 &&
+              ev.GameEnd == NebulaGameEnd.ImpostorWin)
+                ev.SetBlockedIf(true);
+        }
+
+        [Local]
+        void OnExiled(PlayerExiledEvent ev)
+        {
+            if (ev.Player.AmOwner && MyPlayer.Role.Role.Category is RoleCategory.NeutralRole)
+                NebulaGameManager.Instance?.RpcInvokeSpecialWin(NebulaGameEnd.JesterWin, 1 << MyPlayer.PlayerId);
+        }
+
+        [Local]
+        void CheckWin(PlayerCheckExtraWinEvent ev)
+        {
+            if (ev.Player.AmOwner && MyPlayer.Role.Role.Category is RoleCategory.CrewmateRole && 
+              ev.GameEnd == NebulaGameEnd.ImpostorWin)
+                ev.SetWin(true);
+        }
+
+        void RuntimeAssignable.DecorateNameConstantly(ref string name, bool canSeeAllInfo)
+        {
+            if (AmOwner || canSeeAllInfo) name += " Yee".Color(MyRole.UnityColor);
+        }
+    }
+}
